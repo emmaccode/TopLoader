d696cf698d8f366038650122d40a9636c006a8ff
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-01-30 00:07:30 -0500

    added toploader python file!

---
+++ __init__.py
@@ -0,0 +1,40 @@
+print("Initialized")
+import julia
+from julia import Pkg
+class Environment:
+    envname = "env"
+    jl = julia.Julia()
+    pacman = Pkg
+    def __init__(self,envname):
+        """The initialization Function loads a virtual environment."""
+        print("Loading Package Manager")
+        self.pacman = Pkg
+        print("Pkg loaded")
+        self.envname = envname
+        self.pacman.activate(envname)
+        print("Activated Package Environment: " + envname)
+        print("Environment available at: /" + envname)
+    def Add(self, pack):
+        """Adds a package"""
+        self.pacman.add(pack)
+    def Using(self,packagename):
+        """Imports and returns a package"""
+        impstring = "from julia import " + str(packagename)
+        exec(impstring)
+        grabstring = str(packagename)
+        w = eval(grabstring)
+        return(w)
+    def Switch(self,envname):
+        """Allows you to quickly switch environments"""
+        self.pacman.activate(envname)
+        print("Environment created at /" + envname)
+    def List(self):
+        """Lists the current packages inside of this environment"""
+        return(Pkg.installed())
+def Load(file):
+    """Load and environment."""
+    print("Loading file at: " + file)
+    env = Environment(file)
+    print("Environment loaded!")
+    print(Pkg.installed())
+    return(env)
